f67488e1325e9df9e7eb24c97bc518682e305308
Ryan Brennan <ryanbrennanbc1@gmail.com>
2020-10-16 22:53:00 -0700

    start step 2: list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,17 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from 'hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css'],
 })
+//In the same file (HeroesComponent class),
+// define a component property called heroes
+// to expose the HEROES array for binding.
 export class HeroesComponent implements OnInit {
+  heroes = HEROES;
+
   hero: Hero = {
     id: 1,
     name: 'Brennan',
   };
-
   constructor() {}
 
   ngOnInit(): void {}
