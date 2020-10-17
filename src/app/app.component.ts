d1d519558cd94b623c56ec4f1f7c1120f2236bc7
Ryan Brennan <ryanbrennanbc1@gmail.com>
2020-10-16 21:57:32 -0700

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'Hi mi';
+}
+
+// You created the initial application structure using the Angular CLI.
+// You learned that Angular components display data.
+// You used the double curly braces of interpolation to display the app title.
